45da7b3983c0bc3b108a9ec7d8c22edff50f2639
Monibala_S <131789453+MonibalaSelvakumar@users.noreply.github.com>
2024-04-26 08:54:12 +0530

    Add files via upload

---
+++ nodejs/servers.js
@@ -0,0 +1,7 @@
+const os=require('os');
+const path=require('path');
+//const math=require('./math');
+//console.log(math.add(3,3));
+//destructure
+const{add,sub,mul,div}=require('./math');
+console.log(mul(3,3));
