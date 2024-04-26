b70833c3029b56e9a465959381905ff768d7e8ff
Monibala_S <131789453+MonibalaSelvakumar@users.noreply.github.com>
2024-04-26 10:42:42 +0530

    Delete servers.js

--- servers.js
+++
@@ -1,7 +0,0 @@
-const os=require('os');
-const path=require('path');
-//const math=require('./math');
-//console.log(math.add(3,3));
-//destructure
-const{add,sub,mul,div}=require('./math');
-console.log(mul(3,3));
