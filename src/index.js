02106114ceb678ddef7f349cc0513b7434e2dc74
Monibala_S <131789453+MonibalaSelvakumar@users.noreply.github.com>
2024-08-20 14:56:50 +0530

    React-front

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import 'semantic-ui-css/semantic.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import ShopContextProvider from './Context/ShopContext';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <ShopContextProvider>
+      <App />
+    </ShopContextProvider>
 );
